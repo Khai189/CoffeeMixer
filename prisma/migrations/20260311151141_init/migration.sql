4aca7122c83d0a58079f72b2d8740c1b6d3fc328
Khai189 <khmr2024@mymail.pomona.edu>
2026-03-11 08:11:49 -0700

    migration sql added

---
+++ prisma/migrations/20260311151141_init/migration.sql
@@ -0,0 +1,16 @@
+-- AlterTable
+ALTER TABLE "Recipe" ADD COLUMN     "imageUrl" TEXT;
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "authorId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
